db9386806c2a7129df09844e4f3a028a7c6c50ea
Elizabeth Freeman <freemanelizabeth@johndeere.com>
2020-09-15 15:13:56 -0500

    spinner placement just right, recipe cards complete

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,6 @@ class Home extends Component {
     render() {
       return (
         <div className="homepagecontent">
-            <h1>placeholder</h1>
             <div className="spinner">
 <iframe title="spinning choice wheel" src="https://wheeldecide.com/e.php?c1=Tacos&c2=Cheesy+Potato+Soup&c3=Beef+Stroganoff&c4=Chili+Cheese+Dogs&c5=Spaghetti&c6=Cajun+Sausage+Alfredo&c7=Chicken+and+Dumplings&c8=Chicken+and+Noodles&c9=Enchiladas&c10=Fajitas&c11=Bratwurst&c12=Pancakes&c13=Breakfast+Burritos&c14=Omelettes&c15=Eggs+Benedict&c16=Chicken+Pot+Pie&c17=White+Chicken+Chili&c18=Grilled+Cheese+and+Tomato+Soup&c19=Fried+Chicken&c20=Pizza&c21=French+Bread+Pizza&c22=Chinese+Stir+Fry&c23=Pot+Roast&c23=Red+Beans+and+Rice&col=winter&t=Freeman+Family+Dinners&time=5" width="550" height="525" scrolling="no" frameBorder="0"></iframe>
 </div>
